aec9c8b54560c3eacc5d73242f6b1f3de8d58173
Andrew Lee <andrewjkes@hotmail.com>
2024-07-26 11:34:07 +0800

    feat: Create db client and env for local testing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,26 @@
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-export default defineConfig({
-  dialect: "sqlite",
-  schema: "./db/schema.ts",
-  out: "./drizzle",
-  driver: "turso",
-  dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  },
-});
+config({ path: ".env" });
+
+export default defineConfig(
+  process.env.NODE_ENV === "development"
+    ? {
+        dialect: "sqlite",
+        schema: "./db/schema.ts",
+        out: "./drizzle",
+        dbCredentials: {
+          url: "file:test.db",
+        },
+      }
+    : {
+        dialect: "sqlite",
+        schema: "./db/schema.ts",
+        out: "./drizzle",
+        driver: "turso",
+        dbCredentials: {
+          url: process.env.TURSO_DATABASE_URL!,
+          authToken: process.env.TURSO_AUTH_TOKEN,
+        },
+      }
+);
---
+++ lib/db/client.ts
@@ -0,0 +1,23 @@
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+
+const dbUrl =
+  process.env.NODE_ENV === "development"
+    ? "file:test.db"
+    : process.env.TURSO_DATABASE_URL!;
+
+if (dbUrl == undefined) {
+  throw new Error("Missing TURSO_DATABASE_URL environment variable");
+}
+
+const dbAuthToken =
+  process.env.NODE_ENV === "development"
+    ? undefined
+    : process.env.TURSO_AUTH_TOKEN;
+
+const client = createClient({
+  url: dbUrl,
+  authToken: dbAuthToken,
+});
+
+export const db = drizzle(client);
