08f88deb0222c441d55aaf365dd3c5bbc6b2a836
Andrew Lee <andrewjkes@hotmail.com>
2024-07-30 18:18:04 +0800

    Feat: remove unused and add redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+"use client";
+
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <></>
-  );
+  const { user, isLoading } = useUser();
+  if (!isLoading && !user) {
+    redirect('/api/auth/login')
+  } else {
+    redirect('/api/auth/login')
+  }
 }
