421ba68f27907aa42523f92986433f7a682f8283
Andrew Lee <andrewjkes@hotmail.com>
2024-07-31 10:49:28 +0800

    chore(dep): include fontawesome

--- components/NavBarItem.tsx
+++ components/NavBarItem.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
-const NavBarItem = ({ children, className, icon, tabIndex, testId }) => {
+interface NavBarItemProps {
+  children: ReactNode;
+  className?: string;
+  icon?: IconProp;
+  tabIndex?: number;
+  testId?: string;
+}
+
+const NavBarItem: React.FC<NavBarItemProps> = ({ children, className, icon, tabIndex, testId }) => {
   const defaultClasses = "btn btn-ghost";
   const btnClasses = className
     ? `${className} ${defaultClasses}`
@@ -8,6 +18,7 @@ const NavBarItem = ({ children, className, icon, tabIndex, testId }) => {
 
   return (
     <span className="inline-flex items-center">
+      {icon && <FontAwesomeIcon icon={icon} className="mr-3" />}
       <span className={btnClasses} tabIndex={tabIndex} data-testid={testId}>
         {children}
       </span>
