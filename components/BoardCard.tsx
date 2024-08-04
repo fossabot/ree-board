5952575e5f1949f82fcef494a13af69f5e2d2854
Andrew Lee <andrewjkes@hotmail.com>
2024-08-04 23:15:28 +0800

    feat: update boardCard onClick

--- components/BoardCard.tsx
+++ components/BoardCard.tsx
@@ -1,17 +1,25 @@
-import React, { ReactNode, MouseEvent } from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { ReactNode } from "react";
 
 interface BoardCardProps {
   children: ReactNode;
-  onClickAction: (event: MouseEvent<HTMLSpanElement>) => void;
+  boardID?: number;
 }
 
-const BoardCard: React.FC<BoardCardProps> = ({ children, onClickAction }) => {
+const BoardCard: React.FC<BoardCardProps> = ({ children, boardID = -1 }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    if (boardID != -1) {
+      router.push(`/board/${boardID}`);
+    } else {
+      router.push(`/board/create`);
+    }
+  };
   return (
-    <span className="cursor-pointer">
-      <div
-        className="card card-bordered border-primary-content bg-base-120 w-64 h-64 hover:shadow-xl transition-all duration-300 hover:bg-primary-content active:translate-y-1"
-        onClick={onClickAction}
-      >
+    <span className="cursor-pointer" onClick={handleClick}>
+      <div className="card card-bordered border-primary-content bg-base-120 w-64 h-64 hover:shadow-xl transition-all duration-300 hover:bg-primary-content active:translate-y-1">
         <div className="card-body flex items-center justify-center">
           {children}
         </div>
