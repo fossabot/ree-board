699725cdc7dc070e5d489d5bac66de10b6b4c1e0
Andrew Lee <andrewjkes@hotmail.com>
2024-08-06 22:32:20 +0800

    Feat: Add caching on query board data

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,19 +1,39 @@
-import BoardCard from "@/components/BoardCard";
-import NavBar from "@/components/NavBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
+import { unstable_cache } from "next/cache";
+
+import BoardCard from "@/components/BoardCard";
+import NavBar from "@/components/NavBar";
+import { fetchBoards } from "@/lib/db/board";
+import { redirect } from "next/navigation";
+
+const getCachedBoards = unstable_cache(
+  async (userId) => fetchBoards(userId),
+  ["user_boards"]
+);
 
 export default async function Dashboard() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) {
+    redirect("/api/auth/login");
+  }
+
+  const boards = await getCachedBoards(user.id); // replace 1 with actual user ID
   return (
     <>
       <NavBar />
       <BoardCard boardID={-1}>
-        <FontAwesomeIcon
-          className="text-3xl"
-          icon={faSquarePlus}
-        />
+        <FontAwesomeIcon className="text-3xl" icon={faSquarePlus} />
       </BoardCard>
+      {boards.map((board) => (
+        <BoardCard key={board.board.id} boardID={board.board.id}>
+          {board.board.title}
+        </BoardCard>
+      ))}
     </>
   );
 }
