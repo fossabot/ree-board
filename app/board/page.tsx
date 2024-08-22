95820dcc405a5c68fffd254e2ddadd8518446c67
Andrew Lee <andrewjkes@hotmail.com>
2024-08-22 23:40:43 +0800

    feat: Update board list UI and remove unused

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,17 +1,9 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
-import { unstable_cache } from "next/cache";
 import { redirect } from "next/navigation";
 
 import BoardList from "@/components/BoardList";
 import NavBar from "@/components/NavBar";
-import { fetchBoards } from "@/lib/db/board";
-import { boardsSignal } from "@/lib/signal/board";
-
-const getCachedBoards = unstable_cache(
-  async (userId) => fetchBoards(userId),
-  ["user_boards"]
-);
 
 export default async function Boards() {
   const { getUser } = getKindeServerSession();
@@ -21,15 +13,12 @@ export default async function Boards() {
     redirect("/api/auth/login");
   }
 
-  const boards = await getCachedBoards(user.id);
-  boardsSignal.value = boards
-
   return (
     <div className="min-h-screen bg-gray-50">
       <NavBar />
       <main className="container mx-auto mt-8 px-4">
         <h1 className="text-3xl font-bold mb-6">Your Boards</h1>
-        <BoardList />
+        <BoardList userID={user.id} />
       </main>
     </div>
   );
--- components/BoardList.tsx
+++ components/BoardList.tsx
@@ -1,16 +1,22 @@
-"use client"
+"use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import BoardCard from "./BoardCard";
 import CreateBoardForm from "./CreateBoardForm";
 import { boardSignal, boardSignalInitial } from "@/lib/signal/boardSignals";
 import { useSignals } from "@preact/signals-react/runtime";
 
-export default function BoardList() {
+export default function BoardList({ userID }: { userID: string }) {
+  useEffect(() => {
+    boardSignalInitial(userID);
+  }, [userID]);
+
   useSignals();
+
   return (
     <div className="flex flex-wrap gap-4">
-      {boardsSignal.value.map((board) => (
+      <CreateBoardForm />
+      {boardSignal.value.map((board) => (
         <div
           key={0}
           className="w-64 h-32 bg-blue-100 rounded-lg shadow-md flex items-center justify-center hover:bg-blue-200 transition-colors"
@@ -20,7 +26,6 @@ export default function BoardList() {
           </BoardCard>
         </div>
       ))}
-      <CreateBoardForm />
     </div>
   );
 }
