77f8f3f2f10aa7a7aee68be04468ce32daaa42b5
Andrew Lee <andrewjkes@hotmail.com>
2024-09-04 11:22:16 +0800

    feat(ui): lazy loading boardcolumn to improve FCP

--- components/board/BoardGrid.tsx
+++ components/board/BoardGrid.tsx
@@ -1,42 +1,39 @@
-import React from "react";
-import { BoardColumn } from "@/components/board";
 import { PostType } from "@/db/schema";
+import React, { lazy, Suspense } from "react";
+
+const BoardColumn = lazy(() => import("@/components/board/BoardColumn"));
 
 interface BoardGridProps {
   boardID: string;
 }
 
 const BoardGrid: React.FC<BoardGridProps> = ({ boardID }) => {
+  const columns = [
+    { title: "Went Well", postType: PostType.went_well },
+    { title: "To Improve", postType: PostType.to_improvement },
+    { title: "To Discuss", postType: PostType.to_discuss },
+    { title: "Action Items", postType: PostType.action_item },
+  ];
+
   return (
     <div className="flex flex-wrap -mx-2">
-      <div className="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4">
-        <BoardColumn
-          title="Went Well"
-          postType={PostType.went_well}
-          boardID={boardID}
-        />
-      </div>
-      <div className="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4">
-        <BoardColumn
-          title="To Improve"
-          postType={PostType.to_improvement}
-          boardID={boardID}
-        />
-      </div>
-      <div className="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4">
-        <BoardColumn
-          title="To Discuss"
-          postType={PostType.to_discuss}
-          boardID={boardID}
-        />
-      </div>
-      <div className="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4">
-        <BoardColumn
-          title="Action Items"
-          postType={PostType.action_item}
-          boardID={boardID}
-        />
-      </div>
+      {columns.map((column) => (
+        <div key={column.title} className="w-full sm:w-1/2 lg:w-1/4 px-2 mb-4">
+          <Suspense
+            fallback={
+              <div>
+                <span className="loading loading-spinner loading-lg"></span>
+              </div>
+            }
+          >
+            <BoardColumn
+              title={column.title}
+              postType={column.postType}
+              boardID={boardID}
+            />
+          </Suspense>
+        </div>
+      ))}
     </div>
   );
 };
