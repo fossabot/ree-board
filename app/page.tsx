7b214ebd3a0638bada0c059d1a19d96adb05f3bb
Andrew Lee <andrewjkes@hotmail.com>
2024-09-05 15:06:10 +0800

    feat(ui): Improve login page redirect speed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from 'next/navigation';
 
-export default async function Home() {
+export default function Home() {
   const { isAuthenticated } = getKindeServerSession();
-  const isUserAuthenticated = await isAuthenticated();
-  if (!isUserAuthenticated) {
-    redirect('/api/auth/login')
+
+  if (!isAuthenticated()) {
+    redirect('/api/auth/login');
   } else {
-    redirect('/board')
+    redirect('/board');
   }
 }
