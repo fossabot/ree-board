9a5d73137f6b02bc3fb1235ea3dff3ef338a2efe
Andrew Lee <andrewjkes@hotmail.com>
2024-09-11 14:52:49 +0800

    feat(ui): Create landing page for first time user

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from 'next/navigation';
+import {
+  getKindeServerSession,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
   const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
 
-  if (!isAuthenticated()) {
-    redirect('/api/auth/login');
+  if (!isUserAuthenticated) {
+    return (
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Ree Board</h1>
+            <p className="py-6">
+              Retro board build with Next.js and Tailwind CSS
+            </p>
+            <button className="btn btn-outline btn-primary">
+              <LoginLink>Login</LoginLink>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   } else {
-    redirect('/board');
+    redirect("/board");
   }
 }
